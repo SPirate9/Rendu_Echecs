2c0074379b347707d47f88074721d9b8846f6963
SPirate9 <saadshahzad999@gmail.com>
2023-01-19 14:54:24 +0100

    Roi

---
+++ Rendu_final/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author saad
+ *
+ */
+module Rendu_final {
+}
---
+++ Rendu_final/src/piece/Reine.java
@@ -0,0 +1,10 @@
+package piece;
+
+public class Reine {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
